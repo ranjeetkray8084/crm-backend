2c28209110cf0d7ca2c90963f2cec3dd1e8ebf13
Ranjeet Kumar <ranjeetkumarroya23@gmail.com>
2025-09-11 13:12:11 +0530

    now update contact and proprtyDTO

--- src/main/java/com/example/real_estate_crm/Controller/ContactController.java
+++ src/main/java/com/example/real_estate_crm/Controller/ContactController.java
@@ -8,7 +8,7 @@ import com.example.real_estate_crm.model.ContactRequest;
 
 @RestController
 @RequestMapping("/api/contact")
-@CrossOrigin(origins = "*") // Allow React frontend access
+@CrossOrigin(origins = "*") 
 public class ContactController {
 
     @Autowired
@@ -19,11 +19,11 @@ public class ContactController {
         String subject = "New Contact Message from " + request.getName();
         String body = "You have received a new message:\n\n"
                     + "Name: " + request.getName() + "\n"
+                    +"Phone Number: "+request.getNumber() + "\n"
                     + "Email: " + request.getEmail() + "\n\n"
                     + "Message:\n" + request.getMessage();
-
-        // 📨 Send email to your address
-        emailService.sendContactMail("conceptrealty.info@gmail.com", subject, body); 
+        emailService.sendContactMail("support@smartprocares.com", subject, body); 
+        System.out.println("Phone Number: "+request.getNumber());
 
         return "Message sent successfully!";
     }
